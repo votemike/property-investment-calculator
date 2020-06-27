364a92f06030e0021081ac5f3504a6528c9acc82
Michael Gwynne <michael@votemike.co.uk>
2020-06-27 15:33:03 +0100

    Correcting assumptions

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           </header>
           <Form formHandler={setFormData}/>
           <Results formData={formData}/>
-          <div className="has-text-centered is-size-7">Assumptions: The property is in England and follows normal stamp duty costs with the added 3% for second homes. The property has no ground-rent or maintenance charge. The property is a single property and will remain a single property. The purchase will be funded by a bridging loan and will be remortgaged once refurbishment is complete. The mortgage will be interest only with 75% LTV. No other loans will exist apart from the mortgage.</div>
+          <div className="has-text-centered is-size-7">Assumptions: The property is in England and follows normal stamp duty costs with the added 3% for second homes. The property is a single property and will remain a single property. The purchase will be funded by a bridging loan and will be remortgaged once refurbishment is complete. The mortgage will be interest only with 75% LTV. No other loans will exist apart from the mortgage.</div>
         </div>
       </section>
       <footer className="footer">
